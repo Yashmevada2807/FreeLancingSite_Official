25ed12b80069521d135d5f08066bbd75152312a9
Yashmevada2807 <yashmevada07@gmail.com>
2025-06-07 23:30:27 +0530

    add some structure

--- freelancingSite/src/App.jsx
+++ freelancingSite/src/App.jsx
@@ -1,26 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DemoCard from './components/DemoCard'
 
 function App() {
 
 
   return (
     <>
-    <div className='main bg-gray-200 w-full p-4 flex justify-center items-center'>
-      <div className="links  flex justify-around items-center gap-5 rounded-lg shadow-lg  border-black w-fit bg-blue py-3 px-6 ">
-        <button className='p-2 flex items-center justify-center w-fit h-auto shadow-lg rounded-[10px] border '>add</button>
-        <div className="twitterLinks flex gap-5 border-x-2 px-1 border shadow-lg ">
-          <a href="#" ><img className='w-[28px] h-[32px] ' src="https://framerusercontent.com/images/6VsfSOtQYSyGYitFmPQcq42SZbs.svg" alt="" /></a>
-          <a href="#"><img className='w-[28px] h-[32px] ' src="https://framerusercontent.com/images/6VsfSOtQYSyGYitFmPQcq42SZbs.svg" alt="" /></a>
+      <div className='main  w-full p-4 flex justify-center items-center'>
+        <div className="links bg-white/10 backdrop-blur-md z-10 flex justify-around items-center gap-5 rounded-lg shadow-lg  border-black w-fit py-3 px-6 ">
+          <button className='p-2 flex items-center justify-center w-fit h-auto shadow-lg rounded-[10px] border '>add</button>
+          <div className="twitterLinks flex gap-5 px-1  shadow-sm ">
+            <a href="#" ><img className='w-[28px] h-[32px] ' src="https://framerusercontent.com/images/6VsfSOtQYSyGYitFmPQcq42SZbs.svg" alt="" /></a>
+            <a href="#"><img className='w-[28px] h-[32px] ' src="https://framerusercontent.com/images/6VsfSOtQYSyGYitFmPQcq42SZbs.svg" alt="" /></a>
+          </div>
+          <button className='p-2 text-white bg-black flex items-center justify-center w-fit h-auto shadow-lg rounded-[10px] border '>Follow ME</button>
         </div>
-        <button className='p-2 text-white bg-black flex items-center justify-center w-fit h-auto shadow-lg rounded-[10px] border '>Follow ME</button>
-      </div>
-      
-    </div>
 
-    <div className='centerOne w-full h-[500px] border'></div>
+      </div>
+
+      <div className='centerOne w-full whitespace-nowrap overflow-hidden overflow-x-auto scroll-smooth scrollbar-hide h-auto border '>
+        <div className='flex gap-4 px-4 py-6 min-w-fit'>
+          <DemoCard />
+          <DemoCard />
+          <DemoCard />
+          <DemoCard />
+          <DemoCard />
+          <DemoCard />
+          <DemoCard />
+          <DemoCard />
+          <DemoCard />
+        </div>
+      </div>
     </>
   )
 }
---
+++ freelancingSite/src/components/DemoCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DemoCard = () => {
+  return (
+    <div className=' bg-gray-400 rounded-[15px] shadow-lg w-fit h-auto overflow-hidden'>
+        <img className='min-w-[230px] h-[500px]' src="https://endless.design/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frainbow-mobile-1.33fce1de.png&w=1200&q=100" alt="" />
+    </div>
+  )
+}
+
+export default DemoCard
