b4dedb46fa86ab9188c5d809e36a7baacd7f53e5
Yashmevada2807 <yashmevada07@gmail.com>
2025-06-09 00:12:44 +0530

    sm changes

--- freelancingSite/src/components/DemoCard.jsx
+++ freelancingSite/src/components/DemoCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DemoCard = () => {
   return (
     <div className=' bg-gray-400 rounded-[15px] shadow-lg w-fit h-auto overflow-hidden'>
-        <img className='min-w-[230px] h-[500px]' src="https://endless.design/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frainbow-mobile-1.33fce1de.png&w=1200&q=100" alt="" />
+        <img className='min-w-[232px] h-[500px]' src="https://endless.design/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frainbow-mobile-1.33fce1de.png&w=1200&q=100" alt="" />
     </div>
   )
 }
