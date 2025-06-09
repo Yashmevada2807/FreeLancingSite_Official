7a24fd537423d5d3eddf64ed9109041b1784b1af
Karmpatel18 <karmpatel1203@gmail.com>
2025-06-09 20:41:49 +0530

    card component add

--- freelancingSite/src/App.jsx
+++ freelancingSite/src/App.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react'
 import DemoCard from './components/DemoCard'
 import { motion } from 'framer-motion'
+import { Card } from './components/Card'
 
 function App() {
   const scrollRef = useRef(null)
@@ -36,7 +37,7 @@ function App() {
     <>
       <div className="w-full min-h-screen bg-gray-100 text-black  flex flex-col items-center px-4">
         {/* Header */}
-        <div className="w-fit flex items-center justify-center max-w-7xl py-6">
+        {/* <div className="w-fit flex items-center justify-center max-w-7xl py-6">
           <div className="bg-white/10 backdrop-blur-md z-10 flex flex-wrap justify-center sm:justify-between items-center gap-4 rounded-lg shadow-lg border  p-4">
             <button className="p-2 px-4 border rounded-[10px] shadow-lg bg-white/20 hover:bg-white/30">add</button>
             <div className="flex gap-3 items-center">
@@ -45,19 +46,19 @@ function App() {
             </div>
             <button className="p-2 px-4 text-white bg-black rounded-[10px] shadow-lg">Follow ME</button>
           </div>
-        </div>
+        </div> */}
 
         {/* Scroll Section */}
         <div className="relative w-full ">
 
           {/* Left Button */}
           <motion.button
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
+            // whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 0.9 }}
             onClick={scrollLeft}
-            className="absolute left-3 top-1/2  z-10 bg-white/20 backdrop-blur-md hover:bg-white/30 p-2 rounded-full shadow-lg"
+            className="absolute left-3 top-1/2  z-10 bg-white/50 backdrop-blur-md hover:bg-white/60 p-2 rounded-full shadow-lg"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-black dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-black " fill="none" viewBox="0 0 24 24" stroke="currentColor" >
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
           </motion.button>
@@ -65,13 +66,13 @@ function App() {
           {/* Scrollable cards */}
           <div
             ref={scrollRef}
-            className="w-full overflow-x-auto whitespace-nowrap scroll-smooth scrollbar-hide border rounded-lg px-2"
+            className="w-full overflow-x-auto whitespace-nowrap scroll-smooth scrollbar-hide  rounded-lg px-2"
           >
             <motion.div
               className="flex gap-4 py-6 min-w-fit"
-              initial={{ opacity: 0, y: 45 }}
+              initial={{ opacity: 0, y: -25 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1.0 }}
+              transition={{ duration: 1.4 }}
             >
               {cards.map((card, index) => (
               <div key={index} >
@@ -83,16 +84,17 @@ function App() {
 
           {/* Right Button */}
           <motion.button
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
+            // whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 0.9 }}
             onClick={scrollRight}
-            className="absolute right-3 top-1/2  z-10 bg-white/20 backdrop-blur-md hover:bg-white/30 p-2 rounded-full shadow-lg"
+            className="absolute right-3 top-1/2  z-10 bg-white/50 backdrop-blur-md hover:bg-white/60 p-2 rounded-full shadow-lg"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-black dark:text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 text-black ">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
             </svg>
           </motion.button>
         </div>
+        <Card/>
       </div>
     </>
   )
--- freelancingSite/tailwind.config.js
+++ freelancingSite/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        opensans: ['Open+Sans', 'sans-serif'],
+        
+      }
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
